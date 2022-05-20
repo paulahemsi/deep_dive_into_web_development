2f2f6a287ba091a73a2d24a77b6a9aef4eb33073
paula hemsi <paula.campos@loggi.com>
2022-05-20 10:43:31 -0300

    chore(1.4): add array of objects

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -18,40 +18,42 @@ const Content = (props) => {
   console.log(props)
   return(
     <>
-        <Part name={props.part1.name} exercises={props.part1.exercises} />
-        <Part name={props.part2.name} exercises={props.part2.exercises} />
-        <Part name={props.part3.name} exercises={props.part3.exercises} />
+        <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+        <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+        <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   )
 }
 
 const Total = (props) => {
   return(
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-    
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+      
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
