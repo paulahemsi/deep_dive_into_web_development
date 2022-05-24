d9905ac4409798ee64479e2c8f717530506745a6
paula hemsi <paula.campos@loggi.com>
2022-05-24 17:06:36 -0300

    back to simple variables and add allclicks array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,28 +2,27 @@ import { useState } from 'react'
 
 
 const App = () => {
-  const [clicks, setClicks] = useState ({
-    left: 0, right: 0
-  })
- 
-  const handleLeftClick = () => {
-    setClicks({ ...clicks, left: clicks.left + 1})
-  }
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
   
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
   const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1})
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
   
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {clicks.right}
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
