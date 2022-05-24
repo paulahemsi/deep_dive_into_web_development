3fe94e1351afe1edb4c074695d6f9809bc2ea9ab
paula hemsi <paula.campos@loggi.com>
2022-05-24 16:56:46 -0300

    refactor handle functions using spread

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,21 +5,13 @@ const App = () => {
   const [clicks, setClicks] = useState ({
     left: 0, right: 0
   })
-  
+ 
   const handleLeftClick = () => {
-    const newClicks = {
-      ...clicks,
-      left: clicks.left + 1,
-    }
-    setClicks(newClicks)
+    setClicks({ ...clicks, left: clicks.left + 1})
   }
   
   const handleRightClick = () => {
-    const newClicks = {
-      ...clicks,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
+    setClicks({ ...clicks, right: clicks.right + 1})
   }
   
   return (
