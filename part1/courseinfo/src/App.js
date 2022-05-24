1d53d254d325aa9c17ea5cc445a488b3db70134c
paula hemsi <paula.campos@loggi.com>
2022-05-24 16:35:13 -0300

    A more complex state first setup

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,55 +1,35 @@
 import { useState } from 'react'
 
-//The component only uses the counter field of its props. This means we can simplify the component by using destructuring
-// const Display = (props) => {
-//   return (
-//     <div>{props.counter}</div>
-//   )
-// }
-//The function defining the component contains only the return statement, so we can define the function using the more compact form of arrow functions:
-// const Display = ({counter}) => {
-//   return (
-//     <div>{counter}</div>
-//   )
-// }
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({onClick, text}) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
-
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  
   return (
     <div>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text="plus"/>
-      <Button onClick={setToZero} text="zero"/>
-      <Button onClick={decreaseByOne} text="minus"/>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 }
 
 
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => new Date().getFullYear() - age
 
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   )
-// }
+
+
+
+
+
+
+
+
+
 
 // const App = () => {
 //   const name = 'Peter'
