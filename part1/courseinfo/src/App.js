d1d361f86ca757df24ea3a8605b8b634dcc666bd
paula hemsi <paula.campos@loggi.com>
2022-05-25 15:24:02 -0300

    same app but without function that returns a function

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,16 +3,16 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
   
-  const setToValue = (newValue) => () => {
+  const setToValue = (newValue) => {
     console.log('value now', newValue)
     setValue(newValue)
   }
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
   )
 }
