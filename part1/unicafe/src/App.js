6b27d163025eba6242ec5007678bd06df79bb42c
paula hemsi <paula.campos@loggi.com>
2022-06-06 12:26:30 -0300

    fix(unicafe): remove comment

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -48,7 +48,6 @@ const Statistics = ({good, neutral, bad}) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
