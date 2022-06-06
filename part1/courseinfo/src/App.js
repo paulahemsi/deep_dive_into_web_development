aa32e468ccd3bf15fa2107c8e46e9a386eecbc25
paula hemsi <paula.campos@loggi.com>
2022-06-06 12:21:11 -0300

    fix(part1): back to courseinfo finished app version that was overwrited

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,60 @@
-import { useState } from 'react'
+const Header = (props) => {
+  return(
+    <>
+      <h1>{props.name}</h1>
+    </>
+  )
+}
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+const Part = (props) => {
+  return(
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
 
-const Display = props => <div>{props.value}</div>
+const Content = (props) => {
+  return(
+    <>
+        <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+        <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+        <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
 
 const App = () => {
-  const [value, setValue] = useState(10)
-  
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)
-    setValue(newValue)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+        
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  
-  
+
   return (
     <div>
-      <Display value={value}/>
-      <Button handleClick={() => setToValue(1000)} text="thousand"/>
-      <Button handleClick={() => setToValue(0)} text="reset"/>
-      <Button handleClick={() => setToValue(value + 1)} text="increment"/>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
