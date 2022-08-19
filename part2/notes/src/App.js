8ba13cc9482f0080cfa583f8c34e8f5667fcf668
paula hemsi <paula.campos@loggi.com>
2022-08-19 17:37:29 -0300

    filter(notes-part2b): add filtering logic to show important notes

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -4,6 +4,7 @@ import Note from "./components/Note.js"
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("a new note")
+  const [showAll, setShowAll] = useState(true)
   //The event parameter is the event that triggers the call to the event handler function
   const addNote = (event) => {
     //The event handler immediately calls the event.preventDefault() method, which prevents the default action of submitting a form. The default action would, among other things, cause the page to reload.
@@ -25,11 +26,18 @@ const App = () => {
     setNewNote(event.target.value)
   }
   
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+  
    return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) =>
+        {notesToShow.map((note) =>
           <Note key={note.id} note={note}/>
         )}
       </ul>
